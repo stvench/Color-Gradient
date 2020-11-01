61a7e114c9cc524c75dd1f676bf042292b4f1b75
Steven Chen <sc123qweasdzxc@gmail.com>
2020-11-01 13:19:01 -0800

    -Automated to require only galaxy number and wavebands -Catches any error to allow for continued running

--- colorGradient.py
+++ colorGradient.py
@@ -7,13 +7,18 @@ from statistics import mean
 
 
 def read_clusMask(waveband, galNum, group):
+    """
+    Returns a dict containing {color:set of position pairs}
+        - color:    color
+        - position: (i,j) pixel locations
+    """
     # Get Image of Galaxy Arms
     imgClusMask = cv2.imread("{}/{}/{}-K_clusMask-reprojected.png".format(galNum,waveband,galNum))
     dimensions = imgClusMask.shape
     rows = dimensions[0]
     cols = dimensions[1]
     # Store all Pixels of arms
-    allPixelLoc = {}
+    allPixelLoc = dict()
     for i in range(rows):
         for j in range(cols):
             color = tuple(imgClusMask[i,j])
@@ -71,7 +76,7 @@ def read_clusMask(waveband, galNum, group):
     # allPixelLoc(Dictionary) ->  key(color tuples) : value(set of (i,j location) tuples)
     return allPixelLoc, (math.ceil(rows/2),math.ceil(cols/2)), rows, cols # {color : {(i,j) , (i,j)} } , (i,j) }
 
-def get_singleArm(galaxyArms, position):
+def get_closestArm(galaxyArms, position):
     """Returns closest arm to the inputted position (row,col) | (y,x) 
         -Ignores "small" pixels merged into similar color"""
     closestArm, minDist = None, None
@@ -84,15 +89,31 @@ def get_singleArm(galaxyArms, position):
                     closestArm = arm
     return closestArm
 
+def get_largestArm(galaxyArms):
+    """Returns the largest arm in galaxy | mean postion
+            -most pixels, not length"""
+    largestArmPosition = set()
+    largestArmSize = 0
+    for positions in galaxyArms.values():
+        if len(positions)>=largestArmSize:
+            largestArmSize = len(positions)
+            largestArmPosition = positions
+    return largestArmPosition
 
 
-def mergeArms(arms):
-    ### arms should be a iterable of sets of tuples
-    armPosition = set()
-    for arm in arms:
-        armPosition.update(arm)
-    ### singleArm(single set of tuples)    
-    return armPosition # {(i,j) , (i,j)}
+
+
+def mergeArms(waveband1Arm,waveband2Arm):
+    ### Check for overlap (needs at least 50% in one)
+    overlapThreshold = 0.5
+    overlap = waveband1Arm.intersection(waveband2Arm)
+    arm1Overlap = len(overlap)/len(waveband1Arm)
+    arm2Overlap = len(overlap)/len(waveband2Arm)
+    if (arm1Overlap > overlapThreshold) or (arm2Overlap > overlapThreshold):
+        return waveband1Arm.union(waveband2Arm)
+    else:
+        print("{} or {} <= {} Threshold".format(arm1Overlap,arm2Overlap,overlapThreshold))
+        raise TypeError
     
 def arm_to_ArcsCircles(armPosition,center):
     allDists = [math.dist(point,center) for point in armPosition]
@@ -147,6 +168,7 @@ def arm_to_ArcsCircles(armPosition,center):
 
 
 
+
 def simPhis(middle, inner, outer):
     phiList = []
     for phi, i , j in middle.arc:
@@ -196,17 +218,20 @@ def mergedFits(l, waveband1,waveband2,galNum):
 
 
 
-def MAIN(waveband1, waveband2, galNum, position, group):
+
+
+def manualTest(waveband1, waveband2, galNum, position, group):
     pixelLoc1, center, rows, cols = read_clusMask(waveband=waveband1,galNum=galNum,group=group)
-    waveband1Arm      = get_singleArm(galaxyArms=pixelLoc1, position=position)
+    waveband1Arm      = get_closestArm(galaxyArms=pixelLoc1, position=position)
+    
     pixelLoc2, center, rows, cols = read_clusMask(waveband=waveband2,galNum=galNum,group=group)
-    waveband2Arm      = get_singleArm(galaxyArms=pixelLoc2, position=position)
+    waveband2Arm      = get_closestArm(galaxyArms=pixelLoc2, position=position)
     
     
     armPosition                          = mergeArms(arms=[waveband1Arm,waveband2Arm])
     arcsCircles_Positions                = arm_to_ArcsCircles(armPosition=armPosition, center=center)
     for i in range(1,len(arcsCircles_Positions)-1):
-        if (len(arcsCircles_Positions[i].arc) > 50): #and (len(arcsCircles_Positions[i].arc) < 270)
+        if (len(arcsCircles_Positions[i].arc) > 30): #and (len(arcsCircles_Positions[i].arc) < 270)
             curRadiusInfo = arcsCircles_Positions[i]
             phiList = simPhis(curRadiusInfo,arcsCircles_Positions[i-1],arcsCircles_Positions[i+1])
             merged = mergedFits(phiList, waveband1,waveband2,galNum)
@@ -252,7 +277,77 @@ def MAIN(waveband1, waveband2, galNum, position, group):
             plt.show()
             plt.close()
             # break
-            
+
+def automateTest(waveband1, waveband2, galNum):
+    """
+    WAVEBANDS ["u","g","r","i","z"]     
+    Parameters:
+        waveband1 (str) : From WAVEBANDS
+        waveband2 (str) : From WAVEBANDS
+        galNum    (int) :
+    Returns:
+        None (but plots graph)
+    """
+    try:
+        group = True
+        pixelLoc1, center, rows, cols = read_clusMask(waveband=waveband1,galNum=galNum,group=group)
+        waveband1Arm      = get_largestArm(galaxyArms=pixelLoc1)
+        pixelLoc2, center, rows, cols = read_clusMask(waveband=waveband2,galNum=galNum,group=group)
+        waveband2Arm      = get_largestArm(galaxyArms=pixelLoc2)
+        
+        armPosition                          = mergeArms(waveband1Arm=waveband1Arm,waveband2Arm=waveband2Arm)
+        arcsCircles_Positions                = arm_to_ArcsCircles(armPosition=armPosition, center=center)
+        for i in range(1,len(arcsCircles_Positions)-1):
+            if (len(arcsCircles_Positions[i].arc) > 30): #and (len(arcsCircles_Positions[i].arc) < 270)
+                curRadiusInfo = arcsCircles_Positions[i]
+                phiList = simPhis(curRadiusInfo,arcsCircles_Positions[i-1],arcsCircles_Positions[i+1])
+                merged = mergedFits(phiList, waveband1,waveband2,galNum)
+                mergedPhi = []
+                mergedDifs = []
+                for phi, dif in merged: # adjust phi first, then consider overlap
+                    mergedPhi.append(adjustPhi(phi)+360 if adjustPhi(phi)<curRadiusInfo.minPhi else adjustPhi(phi))
+                    mergedDifs.append(dif)
+                unmerged = unmergedFits(curRadiusInfo.arc,waveband1,waveband2,galNum)
+                unmergedPhi = []
+                unmergedDifs = []
+                for phi, dif in unmerged:
+                    unmergedPhi.append(adjustPhi(phi)+360 if adjustPhi(phi)<curRadiusInfo.minPhi else adjustPhi(phi))
+                    unmergedDifs.append(dif)
+                mng = plt.get_current_fig_manager()
+                mng.window.state('zoomed')
+
+                plt.subplot(121)
+                a = np.zeros((rows,cols))
+                for ii, ij in armPosition:
+                    a[ii,ij]=2
+                for ii, ij in curRadiusInfo.circle:
+                    a[ii,ij]=1
+                for phi,ii, ij in curRadiusInfo.arc:
+                    a[ii,ij]=3
+                plt.imshow(a) # origin='lower' for "normal" X/Y axis position
+                plt.text(rows*0.6,cols*0.85,"Min θ: {}".format(curRadiusInfo.minPhi),color="white")
+                plt.text(rows*0.6,cols*0.95,"Max θ: {}".format(curRadiusInfo.maxPhi),color="white")
+                plt.title('Combination')
+
+                plt.subplot(222)
+                plt.plot(unmergedPhi,unmergedDifs)
+                plt.title("Unmerged Radius {}, {}-{}".format(curRadiusInfo.radius,waveband1,waveband2))
+                plt.subplot(224)
+
+                plt.plot(mergedPhi,mergedDifs)
+                plt.xlabel("θ (degrees)",size=15)
+                plt.ylabel("Flux (nanomaggies)",size=15)
+                plt.title("Merged Radius ({},{},{}), {}-{}".format(curRadiusInfo.radius-1,curRadiusInfo.radius,curRadiusInfo.radius+1,waveband1,waveband2))
+                plt.subplots_adjust(hspace=0.3,wspace=0.4)
+                plt.suptitle("Radius: {}".format(curRadiusInfo.radius), size=20)
+                # plt.savefig("{}/{}_{}_{}-{}.pdf".format(galNum,position,curRadiusInfo.radius,waveband1,waveband2))
+                plt.show()
+                plt.close()
+                # break
+    except:
+        print("{} {}-{} Failed".format(galNum,waveband1,waveband2))
+        print("-------------------------------------------")
+
 
 
 class radiusInfo:
@@ -278,24 +373,42 @@ if __name__ == "__main__":
                         --------------REMINDER--------------
     When setting position, uses (ROW,COL) which is equal to (Y,X) in the plotted axis
     """
-
-
-
-
-
+    ############## MANUALLY CHECK POSITIONING
     ### 1237648702986125622
     #
-    # MAIN(waveband1='g',waveband2='i',galNum='1237648702986125622', position=(80,70), group=True)      #G Dark Green
-    # MAIN(waveband1='g',waveband2='i',galNum='1237648702986125622', position=(100,100), group=True)    #G Light Green
+    # manualTest(waveband1='g',waveband2='i',galNum='1237648702986125622', position=(80,70), group=True)      #G Dark Green
+    # manualTest(waveband1='g',waveband2='i',galNum='1237648702986125622', position=(100,100), group=True)    #G Light Green
+
+
+    ### 1237648704586514654
+    #
+    # manualTest(waveband1='g',waveband2='i',galNum='1237648704586514654', position=(90,75), group=True)      #G Top Red
+    # manualTest(waveband1='g',waveband2='i',galNum='1237648704586514654', position=(100,120), group=True)    #G Bottom Orange
+
+
+    ### 1237648705658486867
+    #
+    # manualTest(waveband1='g',waveband2='i',galNum='1237648705658486867', position=(45,45), group=True)      #G Red
 
 
     ### 1237660635996291172
     #
-    # MAIN(waveband1='g',waveband2='i',galNum='1237660635996291172', position=(110,110), group=True)    #G GREEN
-    # MAIN(waveband1='g',waveband2='i',galNum='1237660635996291172', position=(120,140), group=True)    #G PURPLE
-    # MAIN(waveband1='g',waveband2='i',galNum='1237660635996291172', position=(140,120), group=True)    #G ORANGE
+    # manualTest(waveband1='g',waveband2='i',galNum='1237660635996291172', position=(110,110), group=True)    #G GREEN
+    # manualTest(waveband1='g',waveband2='i',galNum='1237660635996291172', position=(120,140), group=True)    #G PURPLE
+    # manualTest(waveband1='g',waveband2='i',galNum='1237660635996291172', position=(140,120), group=True)    #G ORANGE
 
-    
-    # imgClusMask = cv2.imread("1237648702986125622/g/1237648702986125622-K_clusMask-reprojected.png")
+
+
+    ############## AUTOMATE POSITIONING
+    automateTest(waveband1='g',waveband2='i',galNum='1237660635996291172')
+    automateTest(waveband1='g',waveband2='i',galNum='1237648705658486867')
+    automateTest(waveband1='g',waveband2='i',galNum='1237648704586514654')
+    automateTest(waveband1='g',waveband2='i',galNum='1237648702986125622')
+
+
+
+
+
+    # imgClusMask = cv2.imread("1237648705658486867/i/1237648705658486867-K_clusMask-reprojected.png")
     # plt.imshow(imgClusMask)
     # plt.show()
